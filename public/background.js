731e031f45bf76da9951ce0bf1999f4e1bc7264c
dhrubo006 <dhrubo006@gmail.com>
2024-08-01 18:37:44 +0600

    Feature: 1.modified background.js to save urls and collections to the backend dictionary instead of the chrome storage. 2. created a flask backend to save the urls and collections as a dictionary

--- public/background.js
+++ public/background.js
@@ -2,8 +2,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'storeUrl') {
     chrome.storage.local.get({ urls: [] }, (result) => {
       const urls = result.urls;
-      urls.push({ url: message.url, favicon: message.favicon });
+      const newUrlData = { url: message.url, favicon: message.favicon };
+      urls.push(newUrlData);
       chrome.storage.local.set({ urls }, () => {
+        sendToFlaskBackend(newUrlData);
         sendResponse({ status: 'success', url: message.url });
       });
     });
@@ -16,11 +18,30 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (!collections[message.collection]) {
         collections[message.collection] = [];
       }
-      collections[message.collection].push({ url: message.url, favicon: message.favicon });
+      const newCollectionData = { url: message.url, favicon: message.favicon };
+      collections[message.collection].push(newCollectionData);
       chrome.storage.local.set({ collections }, () => {
+        sendToFlaskBackend({ collection: message.collection, ...newCollectionData });
         sendResponse({ status: 'success', collection: message.collection, url: message.url });
       });
     });
     return true; // Indicates that the response will be sent asynchronously
   }
 });
+
+function sendToFlaskBackend(data) {
+  fetch('http://localhost:5000/store_url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
